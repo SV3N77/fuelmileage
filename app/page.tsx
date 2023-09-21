6c5d21e75a75b1f745d3b845564f9521670817e1
SV3N77 <danny.jiang1234@hotmail.com>
2023-09-21 14:28:23 +1000

    update calculation input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import { useState } from "react";
 
 type FuelEconomy = {
   start: number;
@@ -10,54 +9,50 @@ type FuelEconomy = {
 };
 
 export default function Home() {
-  const search = useSearchParams();
-  const start = search.get("Start");
-  const end = search.get("End");
-  const fuel = search.get("Fuel");
+  const [result, setResult] = useState<number>();
+  const [formData, setFormData] = useState<FuelEconomy>();
 
-  const [result, setResult] = useState<Number>(0);
-
-  const [fuelEconomy, setFuelEconomy] = useState<FuelEconomy>({
-    start: start ? parseInt(start as string, 10) : 0,
-    end: end ? parseInt(end as string, 10) : 0,
-    fuel: fuel ? parseInt(fuel as string, 10) : 0,
-  });
-
-  const [submitted, setSubmitted] = useState<Boolean>(false);
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    const travelled = fuelEconomy.end - fuelEconomy.start;
-    const economy = fuelEconomy.fuel / (travelled / 100);
+    const travelled = formData!.end - formData!.start;
+    const economy = formData!.fuel / (travelled / 100);
     setResult(economy);
-
-    setSubmitted(true);
-    console.log(fuelEconomy);
-    console.log(travelled);
-    console.log(economy);
-    console.log(result);
   }
 
   return (
     <main className="container mx-auto mt-10 flex flex-col">
       <section className="container mx-auto flex flex-col gap-4">
-        {submitted ? (
-          <div>Your fuel economy is </div>
-        ) : (
-          <>
-            <div className="font-semibold">
-              Calculate the fuel economy of a vehicle
-            </div>
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col gap-4 items-center"
-            >
-              <CalcInput name={"Start"} id={"Start"} />
-              <CalcInput name={"End"} id={"End"} />
-              <CalcInput name={"Fuel"} id={"Fuel"} />
-              <button type="submit">Calculate</button>
-            </form>
-          </>
-        )}
+        <div className="font-semibold">
+          Calculate the fuel economy of a vehicle
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 items-center"
+        >
+          <CalcInput
+            name={"Start"}
+            id={"start"}
+            metric="Km"
+            formData={formData}
+            setFormData={setFormData}
+          />
+          <CalcInput
+            name={"End"}
+            id={"end"}
+            metric="Km"
+            formData={formData}
+            setFormData={setFormData}
+          />
+          <CalcInput
+            name={"Fuel"}
+            id={"fuel"}
+            metric="L"
+            formData={formData}
+            setFormData={setFormData}
+          />
+          <button type="submit">Calculate</button>
+        </form>
+        {result && <div>Your fuel economy is {result}</div>}
       </section>
     </main>
   );
@@ -68,24 +63,32 @@ export default function Home() {
 type CalcInputType = {
   name: string;
   id: string;
-  // onChange: ;
+  metric: string;
+  formData: FuelEconomy | undefined;
+  setFormData: (e: any) => void;
 };
 
-function CalcInput({ name, id }: CalcInputType) {
+function CalcInput({ name, id, metric, formData, setFormData }: CalcInputType) {
   return (
-    <div className="flex justify-between w-1/5">
+    <div className="flex justify-between w-1/4">
       <label
         htmlFor={id}
         className="block text-gray-700 text-sm font-bold px-3 py-2"
       >
         {name}:
       </label>
-      <input
-        type="text"
-        id={id}
-        name={id}
-        className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-right"
-      />
+      <div className="flex  w-1/2">
+        <input
+          type="text"
+          id={id}
+          name={id}
+          onChange={(e) => setFormData({ ...formData, [id]: e.target.value })}
+          className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-right"
+        />
+        <div className="block text-gray-700 text-sm font-bold px-3 py-2">
+          {metric}
+        </div>
+      </div>
     </div>
   );
 }
